2e36a9592883940461299eebdf4534af9953be62
sergio albarran <sergio@myvoga.com>
2016-03-25 09:04:14 -0600

    Interface +
    NA

--- Pizza/Cocina.swift
+++ Pizza/Cocina.swift
@@ -10,13 +10,46 @@ import UIKit
 
 class Cocina: UIViewController {
 
+    @IBOutlet weak var timer: UILabel!
     
+    @IBOutlet weak var countd: UILabel!
     override func viewWillAppear(animated: Bool) {
         
     }
+    
+    var timerCounter:NSTimeInterval!
+    
+    func stringFromTimeInterval(interval: NSTimeInterval) -> String {
+        let interval = Int(interval)
+        let seconds = interval % 60
+        let minutes = (interval / 60) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func startTimer(hour:Double) {
+        timerCounter = NSTimeInterval(hour * 60 * 60)
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "onTimer:", userInfo: nil, repeats: true)
+    }
+    
+    func onTimer(timer:NSTimer!) {
+        // Here is the string containing the timer
+        // Update your label here
+        countd.text=(stringFromTimeInterval(timerCounter))
+        timerCounter!--
+}
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = true
+        startTimer(0.50)
+        
+        let todaysDate:NSDate = NSDate()
+        let dateFormatter:NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        let DateInFormat:String = dateFormatter.stringFromDate(todaysDate)
+        timer.text = DateInFormat
+        
         // Do any additional setup after loading the view.
     }
 
